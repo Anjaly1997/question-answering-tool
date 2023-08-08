ff8b7d087772025ba74061e222c913c6c1930cba
Anjaly1997 <anjaly1997soman@gmail.com>
2023-08-08 12:42:44 +0200

    Implement InputText component

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function InputText() {
+
+   const [text, setText] = useState('');
+   const [question,setQuestion] = useState(); 
+   
+   const handleSubmit = () => {
+    console.log(text);
+    console.log(question); 
+   }
+
   return (
-    <div>InputText</div>
+    <div>
+        <textarea 
+        placeholder='Enter the text here...'
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+        />
+        <input 
+        placeholder='Enter your Question' 
+        value={question}
+        onChange={(e)=>setQuestion(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
   )
 }
 
