5fb8625cf7cf52734ad2415f00e6b3222316d5cd
Anjaly1997 <anjaly1997soman@gmail.com>
2023-08-08 19:10:30 +0200

    Integrate Axios for API request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import InputText from './components/InputText';
 import DisplayAnswer from './components/DisplayAnswer';
+import { useState } from 'react';
+import axios from 'axios';
 function App() {
+
+const [answer, setAnswer] = useState('');
+
+const handleUserInput = ({text,question}) =>{
+  axios
+  .post("https://api-inference.huggingface.co/models/deepset/roberta-base-squad2", {
+    question,
+    context: text,
+  })
+  .then((response) => {
+    // Update the answer state with the relevant answer
+    setAnswer(response.data["answer"]);
+  })
+  .catch((error) => {
+    // Handle errors if any
+    console.error("Error fetching answer:", error);
+  });
+};
+
   return (
     <div className="App">
       <h1>Question-anwsering-tool</h1>
-      <InputText/>
-      <DisplayAnswer/>
+      <InputText onSubmit = {handleUserInput}/>
+      <DisplayAnswer answer={answer}/>
     </div>
   );
 }
--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,28 +1,35 @@
 import React, { useState } from 'react'
 
-function InputText() {
+function InputText({onSubmit}) {
 
    const [text, setText] = useState('');
    const [question,setQuestion] = useState(); 
    
    const handleSubmit = () => {
-    console.log(text);
-    console.log(question); 
-   }
+    onSubmit({text, question});
+   };
 
   return (
     <div>
+        
         <textarea 
+        rows='8'
+        cols= '80'
         placeholder='Enter the text here...'
         value={text}
         onChange={(e)=>setText(e.target.value)}
         />
+        <br />
+        
         <input 
+        style={{ width: '300px' }} 
         placeholder='Enter your Question' 
         value={question}
         onChange={(e)=>setQuestion(e.target.value)}
         />
-        <button onClick={handleSubmit}>Submit</button>
+        <br />
+        <button onClick={handleSubmit}>Submit</button> <span>    </span>
+        <button>Re-run</button>
     </div>
   )
 }
