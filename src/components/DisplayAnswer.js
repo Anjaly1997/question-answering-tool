2e0ec4e1c27fbad6ba8bc606c37387faf4a8b466
Anjaly1997 <anjaly1997soman@gmail.com>
2023-08-11 19:36:13 +0200

    Styling DisplayAnswer.js

--- src/components/DisplayAnswer.js
+++ src/components/DisplayAnswer.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
 
-function DisplayAnswer({answer}) {
+
+const DisplayAnswer = ({ answer }) => {
   return (
-    <div>
-        {answer && <p>Answer: {answer}</p>}
-        {!answer && <p>No answer found.</p>}
-    </div>
-  )
-}
+    <Box sx={{ minWidth: 275, maxWidth: '800px', margin: '20px auto' }}>
+      <Card variant="outlined" sx={{ backgroundColor: '#141920' }}>
+        <CardContent sx={{ padding: '20px', textAlign: 'left' }}>
+          {answer === null ? (
+            <CircularProgress />
+          ) : answer ? (
+            <Typography variant="body1" color="#ffffff">
+              {answer}
+            </Typography>
+          ) : (
+            <Typography variant="body1" color="text.secondary">
+              No answer found.
+            </Typography>
+          )}
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
 
-export default DisplayAnswer
+export default DisplayAnswer;
