c97119766b9721cd877a68c77b92e1e386c82707
Anjaly1997 <anjaly1997soman@gmail.com>
2023-08-11 19:55:46 +0200

    Adding Like, Dislike and Copy f

--- src/components/DisplayAnswer.js
+++ src/components/DisplayAnswer.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import FileCopyOutlinedIcon from '@mui/icons-material/FileCopyOutlined';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
 import CircularProgress from '@mui/material/CircularProgress';
-
+import { Tooltip } from '@mui/material';
 
 const DisplayAnswer = ({ answer }) => {
+  const [likeClicked, setLikeClicked] = useState(false);
+  const [dislikeClicked, setDislikeClicked] = useState(false);
+  const [copyClicked, setCopyClicked] = useState(false);
+
+  const handleLike = () => {
+    setLikeClicked(true);
+    setDislikeClicked(false); // Disable dislike when like is clicked
+  };
+
+  const handleDislike = () => {
+    setDislikeClicked(true);
+    setLikeClicked(false); // Disable like when dislike is clicked
+  };
+
+  const copyToClipboard = (text) => {
+    const textField = document.createElement('textarea');
+    textField.value = text;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  };
+
+  const handleCopyAnswer = () => {
+    if (answer) {
+      copyToClipboard(answer);
+      setCopyClicked(true);
+    }
+  };
+
   return (
     <Box sx={{ minWidth: 275, maxWidth: '800px', margin: '20px auto' }}>
       <Card variant="outlined" sx={{ backgroundColor: '#141920' }}>
@@ -14,7 +51,7 @@ const DisplayAnswer = ({ answer }) => {
           {answer === null ? (
             <CircularProgress />
           ) : answer ? (
-            <Typography variant="body1" color="#ffffff">
+            <Typography variant="h6" color="#ffffff">
               {answer}
             </Typography>
           ) : (
@@ -22,6 +59,30 @@ const DisplayAnswer = ({ answer }) => {
               No answer found.
             </Typography>
           )}
+          <div
+            style={{
+              marginTop: '10px',
+              display: 'flex',
+              justifyContent: 'right',
+              alignItems: 'center',
+            }}
+          >
+            <div>
+              <IconButton onClick={handleLike} color="success" disabled={dislikeClicked}>
+                {likeClicked ? <ThumbUpIcon /> : <ThumbUpOutlinedIcon />}
+              </IconButton>
+              <IconButton onClick={handleDislike} color="error" disabled={likeClicked}>
+                {dislikeClicked ? <ThumbDownIcon /> : <ThumbDownOutlinedIcon />}
+              </IconButton>
+            </div>
+            {answer && (
+              <Tooltip title="Copy Answer">
+                <IconButton onClick={handleCopyAnswer} color="primary">
+                  {copyClicked ? <FileCopyIcon /> : <FileCopyOutlinedIcon />}
+                </IconButton>
+              </Tooltip>
+            )}
+          </div>
         </CardContent>
       </Card>
     </Box>
